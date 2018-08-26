203553629b7e1d055619574118d9dc197f541d49
benxiao <benxiao@wacai.com>
2018-08-26 22:12:04 +0800

    修改网站

--- src/main/java/com/zhengzhou/zhiyuangong/shower/controller/HomeController.java
+++ src/main/java/com/zhengzhou/zhiyuangong/shower/controller/HomeController.java
@@ -6,14 +6,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/main")
-	public String getHome() {
-		return "main";
-	}
-
-	@RequestMapping("/index")
-	public String getIndex() {
+	@RequestMapping("index")
+	public String home() {
 		return "index";
 	}
 
+	@RequestMapping("main")
+	public String main() {
+		return "main";
+	}
+
+	@RequestMapping("us")
+	public String us() {
+		return "us";
+	}
+
+	@RequestMapping("product")
+	public String product() {
+		return "product";
+	}
+
+	@RequestMapping("problem")
+	public String problem() {
+		return "problem";
+	}
+
+	@RequestMapping("contact")
+	public String contact() {
+		return "contact";
+	}
 }
